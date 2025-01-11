50f8b895f7d197180bf61f62280ca892f266ac1c
Osvaldo Valdivia <ovaldovia983@gmail.com>
2025-01-11 14:13:26 -0600

    concediendo acceso

--- bd.py
+++ bd.py
@@ -1,1 +1,6 @@
 print("Este es el archivo para el codigo de bases de datos")
+
+password = "holaPython"
+passwordUser = input("Ingresa un password: ")
+
+print("Tienes el acceso" if passwordUser == password else "NO tienes acceso")
